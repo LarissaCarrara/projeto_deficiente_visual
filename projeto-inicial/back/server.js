21d5ade4b730a6246ff4bf54e47df0e16dbd450d
Lucas A. Camacho <111870284+LcsCamacho@users.noreply.github.com>
2023-06-07 20:45:42 -0300

    ajustes

--- projeto-inicial/back/server.js
+++ projeto-inicial/back/server.js
@@ -1,15 +1,16 @@
 //config padrao
 const express = require("express");
 const cors = require("cors"); //segurança do back
-
-const iaRouters = require("./ia_route");
 const app = express();
 
 app.use(cors());
+
+const iaRouters = require("./ia_route");
+
 app.use(express.json());
 
 app.use(iaRouters);
 
-app.listen(3001, ()=>{
-    console.log("rodando");
-})
+app.listen(3001, () => {
+  console.log("rodando");
+});
--- projeto-inicial/front/style/script.js
+++ projeto-inicial/front/style/script.js
@@ -5,11 +5,6 @@ const captureBtn = document.querySelector("#capture-btn");
 const table = document.querySelector("table");
 const produto = document.querySelector("#produto");
 
-// const tab_produto = document.querySelector('#trproduto');
-// const tab_sabor = document.querySelector('#sabor');
-// const tab_preco = document.querySelector('#preco');
-// const tab_descricao = document.querySelector('#descricao');
-// const tab_marca = document.querySelector('#marca');
 const tbody = document.querySelector("tbody");
 const downloadLink = document.querySelector("#download-link");
 
@@ -64,17 +59,10 @@ captureBtn.addEventListener("click", () => {
   document.querySelector(".div_img").appendChild(img);
 });
 
-fetch("C:/Users/Aluno/Downloads/teste.png").then((res) => console.log(res));
-
-function buscar() {
+async function buscar() {
   const arq = document.querySelector("#file");
   const data = new FormData();
   data.append("img", arq.files[0], arq.files[0].name);
-
-  //   if (!blobTeste) {
-  //     console.log("Nenhuma imagem capturada.");
-  //     return;
-  //   }
 
   const file = new File([blobTeste], "captured_image.png");
 
@@ -88,151 +76,139 @@ function buscar() {
     method: "POST",
     body: data,
   };
-  fetch(
+  const res1 = await fetch(
     "https://inteligenciaartificialunisal-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/683de454-c974-4889-bd6a-9aede14acba2/detect/iterations/Iteration1/image",
     options
-  )
-    .then((resp) => {
-      if (resp.ok) {
-        return resp.json();
+  );
+  const res1JSON = await res1.json();
+
+  // Classifica as previsões em ordem decrescente de probabilidade
+  res1JSON.predictions.sort((a, b) => b.probability - a.probability);
+  tbody.innerHTML = tbodyClone.innerHTML;
+  const tab_produto = document.querySelector("#trproduto");
+  const tab_sabor = document.querySelector("#sabor");
+  const tab_preco = document.querySelector("#preco");
+  const tab_descricao = document.querySelector("#descricao");
+  const tab_marca = document.querySelector("#marca");
+  if (resp != undefined) {
+    // tbody.innerHTML = tbodyClone.innerHTML
+    table.style.display = "block";
+
+    let x = 0;
+    let y = 0;
+    let z = 0;
+    let sabor = false;
+    let marca = false;
+    let produto = false;
+
+    res1JSON.predictions.forEach((element, index) => {
+      if (sabor && marca && produto) return;
+      if (x == 0) {
+        if (element.tagName.includes("Produto")) {
+          const tdproduto = document.createElement("td");
+          let frase = [];
+          frase = element.tagName.split(" ");
+          frase.slice(0, 1);
+
+          tdproduto.innerHTML = frase.join(" ");
+          tdproduto.className = "t";
+
+          const tdprob1 = document.createElement("td");
+          tdprob1.innerHTML = (element.probability * 100).toFixed(3) + "%";
+
+          tdprob1.classList = "t";
+
+          dados.produto = tdproduto.innerHTML;
+
+          tab_produto.appendChild(tdproduto);
+          tab_produto.appendChild(tdprob1);
+          x = 1;
+          return;
+        }
+      }
+      if (y == 0) {
+        if (element.tagName.includes("Marca")) {
+          const tdmarca = document.createElement("td");
+          let frase = [];
+          frase = element.tagName.split(" ");
+          frase.slice(0, 1);
+
+          tdmarca.innerHTML = frase.join(" ");
+          tdmarca.classList = "t";
+          const tdprob3 = document.createElement("td");
+          tdprob3.innerHTML = (element.probability * 100).toFixed(3) + "%";
+          tdprob3.classList = "t";
+
+          tab_marca.appendChild(tdmarca);
+          tab_marca.appendChild(tdprob3);
+
+          dados.marca = tdmarca.innerHTML;
+          marca = true;
+          y = 1;
+          return;
+        }
+      }
+      if (z == 0) {
+        if (element.tagName.includes("Sabor")) {
+          const tdsabor = document.createElement("td");
+          const tdprob2 = document.createElement("td");
+          let frase = [];
+          frase = element.tagName.split(" ");
+          frase.slice(0, 1);
+          tdsabor.innerHTML = frase.join(" ");
+          tdsabor.className = "t";
+          tdprob2.innerHTML = (element.probability * 100).toFixed(3) + "%";
+          tdprob2.classList = "t";
+
+          tab_sabor.appendChild(tdsabor);
+          tab_sabor.appendChild(tdprob2);
+
+          dados.sabor = tdsabor.innerHTML;
+          sabor = true;
+          z = 1;
+          return;
+        }
+      }
+    });
+    const res2 = await fetch("https://ia-k7lc.onrender.com/listar");
+    const res2JSON = await res2.json();
+    console.log(res2JSON);
+    res2JSON.forEach((element) => {
+      const tdpreco = document.createElement("td");
+      tdpreco.innerHTML = "R$ " + element.preco.toFixed(2);
+      tdpreco.className = "t";
+
+      const tdvazio = document.createElement("td");
+      tdvazio.innerHTML = "&nbsp;";
+
+      const tddescricao = document.createElement("td");
+      tddescricao.innerHTML = element.descricao;
+      tddescricao.classList = "t";
+
+      if ("speechSynthesis" in window) {
+        // Cria um novo objeto SpeechSynthesisUtterance
+        var mensagem = new SpeechSynthesisUtterance();
+
+        // Define o texto a ser lido em voz alta
+        mensagem.text = element.descricao;
+
+        // Fala a mensagem
+        speechSynthesis.speak(mensagem);
       } else {
-        throw new Error("Erro na solicitação");
+        console.log(
+          "A API de síntese de fala não é suportada neste navegador."
+        );
       }
-    })
-    .then((resp) => {
-      // Classifica as previsões em ordem decrescente de probabilidade
-      resp.predictions.sort((a, b) => b.probability - a.probability);
-      tbody.innerHTML = tbodyClone.innerHTML;
-      const tab_produto = document.querySelector("#trproduto");
-      const tab_sabor = document.querySelector("#sabor");
-      const tab_preco = document.querySelector("#preco");
-      const tab_descricao = document.querySelector("#descricao");
-      const tab_marca = document.querySelector("#marca");
-      if (resp != undefined) {
-        // tbody.innerHTML = tbodyClone.innerHTML
-        table.style.display = "block";
 
-        let x = 0;
-        let y = 0;
-        let z = 0;
-        let sabor = false;
-        let marca = false;
-        let produto = false;
+      const tdvazio2 = document.createElement("td");
+      tdvazio2.innerHTML = "&nbsp;";
 
-        resp.predictions.forEach((element, index) => {
-          if (sabor && marca && produto) return;
-          if (x == 0) {
-            if (element.tagName.includes("Produto")) {
-              const tdproduto = document.createElement("td");
-              let frase = [];
-              frase = element.tagName.split(" ");
-              frase.slice(0, 1);
-
-              tdproduto.innerHTML = frase.join(" ");
-              tdproduto.className = "t";
-
-              const tdprob1 = document.createElement("td");
-              tdprob1.innerHTML = (element.probability * 100).toFixed(3) + "%";
-
-              tdprob1.classList = "t";
-
-              dados.produto = tdproduto.innerHTML;
-
-              tab_produto.appendChild(tdproduto);
-              tab_produto.appendChild(tdprob1);
-              x = 1;
-              return;
-            }
-          }
-          if (y == 0) {
-            if (element.tagName.includes("Marca")) {
-              const tdmarca = document.createElement("td");
-              let frase = [];
-              frase = element.tagName.split(" ");
-              frase.slice(0, 1);
-
-              tdmarca.innerHTML = frase.join(" ");
-              tdmarca.classList = "t";
-              const tdprob3 = document.createElement("td");
-              tdprob3.innerHTML = (element.probability * 100).toFixed(3) + "%";
-              tdprob3.classList = "t";
-
-              tab_marca.appendChild(tdmarca);
-              tab_marca.appendChild(tdprob3);
-
-              dados.marca = tdmarca.innerHTML;
-              marca = true;
-              y = 1;
-              return;
-            }
-          }
-          if (z == 0) {
-            if (element.tagName.includes("Sabor")) {
-              const tdsabor = document.createElement("td");
-              const tdprob2 = document.createElement("td");
-              let frase = [];
-              frase = element.tagName.split(" ");
-              frase.slice(0, 1);
-              tdsabor.innerHTML = frase.join(" ");
-              tdsabor.className = "t";
-              tdprob2.innerHTML = (element.probability * 100).toFixed(3) + "%";
-              tdprob2.classList = "t";
-
-              tab_sabor.appendChild(tdsabor);
-              tab_sabor.appendChild(tdprob2);
-
-              dados.sabor = tdsabor.innerHTML;
-              sabor = true;
-              z = 1;
-              return;
-            }
-          }
-        });
-        fetch("https://ia-k7lc.onrender.com/listar")
-          .then((resp) => {
-            return resp.json();
-          })
-          .then((data) => {
-            data.forEach((element) => {
-              const tdpreco = document.createElement("td");
-              tdpreco.innerHTML = "R$ " + element.preco.toFixed(2);
-              tdpreco.className = "t";
-
-              const tdvazio = document.createElement("td");
-              tdvazio.innerHTML = "&nbsp;";
-
-              const tddescricao = document.createElement("td");
-              tddescricao.innerHTML = element.descricao;
-              tddescricao.classList = "t";
-
-              if ("speechSynthesis" in window) {
-                // Cria um novo objeto SpeechSynthesisUtterance
-                var mensagem = new SpeechSynthesisUtterance();
-
-                // Define o texto a ser lido em voz alta
-                mensagem.text = element.descricao;
-
-                // Fala a mensagem
-                speechSynthesis.speak(mensagem);
-              } else {
-                console.log(
-                  "A API de síntese de fala não é suportada neste navegador."
-                );
-              }
-
-              const tdvazio2 = document.createElement("td");
-              tdvazio2.innerHTML = "&nbsp;";
-
-              tab_preco.appendChild(tdpreco);
-              tab_preco.appendChild(tdvazio);
-              tab_descricao.appendChild(tddescricao);
-              tab_descricao.appendChild(tdvazio2);
-            });
-          })
-          .catch((err) => console.log(err));
-      }
-    })
-    .catch((err) => console.log(err));
+      tab_preco.appendChild(tdpreco);
+      tab_preco.appendChild(tdvazio);
+      tab_descricao.appendChild(tddescricao);
+      tab_descricao.appendChild(tdvazio2);
+    });
+  }
 }
 
 function mostrar() {
